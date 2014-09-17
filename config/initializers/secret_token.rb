6aece1a5adb6051b4e66532bafe27d3dba4e1314
ioandanpopa <popa.dan@agilefreaks.com>
2014-09-17 21:55:14 +0300

    Initializes app structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '3b1b38f7f29f443515933bf60fc9b5d0aab2fc69938a9f5bc49a2a392e093775192daa2ec6ecd18d8a27bf68356cddfe260d82ee19492dd653a39422c7e3f18f'
